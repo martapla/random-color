27efad90d508211eb59efd039fdcacd136fbf77c
martapla <martaplanella@hotmail.com>
2024-07-25 23:30:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+
+import { useState, useEffect } from "react";
+import "./App.css";
+import { HexColorPicker } from "react-colorful";
+
+export default function App() {
+ 
+  const [color, setColor] = useState("#ffffff");
+
+  useEffect(() => {
+    getRandomColor();
+  },[]); 
+
+  // convert the hex color to rgb
+ 
+  const hexToRgb = (hex) => {
+    const bigint = parseInt(hex.slice(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return `${r} ${g} ${b}`;
+  };
+
+  // const rgbColor = "255 255 255"; // it can be done in one line
+  const rgbColor = hexToRgb(color);
+
+  // get a random color from thecolorapi.com in json format using this URL
+  const getRandomColor = async () => {
+    try {
+      const response = await fetch(`https://www.thecolorapi.com/random?format=json`);
+      const data = await response.json();
+      setColor(data.hex.value);
+    } catch (error) {
+      console.error("Error fetching color:", error);
+    }
+  };
+
+  const handleColorChange = (color) => {
+    setColor(color);
+  };
+
+
+  return (
+    <main>
+      
+      {/* Render an HEX color picker component here */}
+      <HexColorPicker color={color} onChange={handleColorChange} />
+      {/* Check docs https://github.com/omgovich/react-colorful */}
+      <button onClick={getRandomColor}>Random Color</button>
+      <div>The color in HEX is {color}</div>
+      <div>The color in RGB is {rgbColor}</div>
+    </main>
+  );
+}
